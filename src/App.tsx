7ab90cce7378493fa55b3f44146c92913c955500
Karla Lucia Böttcher <karla.boettcher@posteo.de>
2025-11-29 17:36:47 +0100

    defined the store, styling adjustments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
 import zustandLogo from "./assets/zustand.svg";
 import "./App.css";
+import { useCounterStore } from "./store";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const count = useCounterStore((state) => state.count);
   return (
     <>
-        <a href="https://github.com/pmndrs/zustand" target="_blank">
-          <img src={zustandLogo} className="logo react" alt="React logo" />
-        </a>
+      <a href="https://github.com/pmndrs/zustand" target="_blank">
+        <img src={zustandLogo} className="logo react" alt="React logo" />
+      </a>
       <h1>Learning Zustand</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-        </p>
+        <h2>
+          <SomeComponent count={count} />
+        </h2>
       </div>
     </>
   );
 }
+
+const SomeComponent = ({ count }: { count: number }) => {
+  return <>{count}</>;
+};
 
 export default App;
